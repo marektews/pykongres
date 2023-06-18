d2c4546f523529436f1bbaec08c53a1de6010123
Marek Tews <marek.tews@gmail.com>
2023-06-18 09:38:51 +0200

    Wstawianie danych w szablon identyfikatora autokarowego i renderowanie do PDF

--- api/ia/Download.py
+++ api/ia/Download.py
@@ -1,6 +1,6 @@
 import io
 from flask import current_app, send_file
-from sql import SRA, Zbory
+from sql import SRA, Zbory, Rozklad, Sektory, Terminale
 from svglib.svglib import svg2rlg
 from svglib.fonts import register_font
 from reportlab.graphics import renderPDF
@@ -10,21 +10,27 @@ from .generate import gen
 def _ia_download(sra_id):
     try:
         sra = SRA.query.filter_by(id=sra_id).one()
-        zbor = Zbory.query.filter_by(id=sra.zbor_id).first()
+        congregation = Zbory.query.filter_by(id=sra.zbor_id).one()
+        rja = Rozklad.query.filter_by(sra_id=sra.id).one()
+        sector = Sektory.query.filter_by(id=rja.sektor_id).one()
+        terminal = Terminale.query.filter_by(id=sector.tid).one()
 
-        svg = gen(sra, zbor)
+        all_departures = Rozklad.query.filter_by(sektor_id=rja.sektor_id).order_by(Rozklad.d1).all()
+        order = [item.sra_id for item in all_departures]
+        tura = order.index(rja.sra_id) + 1
 
-        register_font("Roboto", "Roboto-Regular.ttf")
-        register_font("Roboto-Bold", "Roboto-Bold.ttf")
+        svg = gen(congregation, rja, sector, terminal, str(tura))
+
+        register_font("Lato-Bold", "Lato-Bold.ttf")
         fd_svg = io.BytesIO()
         fd_svg.write(svg)
         fd_svg.seek(0)
         drawing = svg2rlg(fd_svg)
         fd_pdf = io.BytesIO()
-        renderPDF.drawToFile(drawing, fd_pdf, autoSize=0)
+        renderPDF.drawToFile(drawing, fd_pdf)
         fd_pdf.seek(0)
-        return send_file(fd_pdf, mimetype="application/pdf", as_attachment=True, download_name="identyfikator-parkingowy.pdf")
+        return send_file(fd_pdf, mimetype="application/pdf", as_attachment=True, download_name="identyfikator-autokaru.pdf")
 
     except Exception as e:
-        current_app.logger.error(f"SRA download pass id: exception: {e}")
+        current_app.logger.error(f"IA download pass id: exception: {e}")
         return f"{e}", 500
--- api/ia/generate.py
+++ api/ia/generate.py
@@ -2,8 +2,45 @@ import xml.etree.ElementTree as ET
 ET.register_namespace("svg", "http://www.w3.org/2000/svg")
 
 
-def gen(sra, zbor):
-    tree = ET.parse("autokar-pass-id-template-1.svg")
+def gen(congregation, rja, sector, terminal, tura):
+    tree = ET.parse("identyfikator-autokaru-template.svg")
     root = tree.getroot()
 
+    # terminal: Łazienkowska (D) | Torwar (T)
+    elem = root.find(".//*[@id='terminal']")
+    elem.text = terminal.name
+
+    # numer sektora
+    elem = root.find(".//*[@id='sektor']")
+    elem.text = _sector_number(sector.name)
+
+    # sektor + tura
+    elem = root.find(".//*[@id='identyfikator']")
+    elem.text = sector.name.replace('x', tura)
+
+    # nazwa zboru
+    elem = root.find(".//*[@id='congregation']")
+    elem.text = congregation.name
+
+    # podstawienia
+    elem = root.find(".//*[@id='arrive1']")
+    elem.text = rja.a1.strftime("%H:%M")
+    elem = root.find(".//*[@id='arrive2']")
+    elem.text = rja.a2.strftime("%H:%M")
+    elem = root.find(".//*[@id='arrive3']")
+    elem.text = rja.a3.strftime("%H:%M")
+
+    # odjazdy
+    elem = root.find(".//*[@id='departure1']")
+    elem.text = rja.d1.strftime("%H:%M")
+    elem = root.find(".//*[@id='departure2']")
+    elem.text = rja.d2.strftime("%H:%M")
+    elem = root.find(".//*[@id='departure3']")
+    elem.text = rja.d3.strftime("%H:%M")
+
     return ET.tostring(root)
+
+
+def _sector_number(name):
+    i = name.index('x')
+    return name[i+1:]
